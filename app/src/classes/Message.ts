16380976a116e7cb4544ede69c75be9c39ddd7b7
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-02-19 22:56:31 +0100

    Corrected attribute types

--- app/src/classes/Message.ts
+++ app/src/classes/Message.ts
@@ -1,11 +1,12 @@
 import { User } from "./User";
+import { Room } from "./Room";
 
 export class Message{
     public readonly from: User;
     public readonly content: String;
-    public readonly to: [User, String];
+    public readonly to: [User, Room];
 
-    constructor(from: User, content: String, to: [User, String] = null){
+    constructor(from: User, content: String, to: [User, Room] = null){
         this.from = from;
         this.content = content;
         this.to = to;
