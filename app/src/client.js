accc65149c285443b7e57fb79b0dc0145bb4104c
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-02-25 17:58:35 +0100

    Fixed spacing on secret message (ssshhh)

--- app/src/client.js
+++ app/src/client.js
@@ -5,7 +5,7 @@ app.start({
 	target: document.querySelector('#app')
 }).then(() => {
 	console.log("%c🔥 CAELI is UP 🔥", "font-size: 30px; font-weight: bold;");
-	console.log("%cNoto con piacere che ti piace smanettare.\nSe te ne intendi di programmazione web sei\nil benvenuto a contribuire a questo progetto su GitHub!", "color: blue");
+	console.log("%cNoto con piacere che ti piace smanettare.\nSe te ne intendi di programmazione web sei\nil benvenuto a contribuire a questo progetto\nsu GitHub!", "color: blue");
 }).catch(() => {
 	console.error("Oh f**k, errors!");
 });
