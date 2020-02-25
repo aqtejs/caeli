960282310c1b82604e3c72c4dd5775e268850aa3
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-02-25 18:02:32 +0100

    Emptied layout

--- app/src/routes/_layout.svelte
+++ app/src/routes/_layout.svelte
@@ -1,24 +1,3 @@
-<script>
-	import Nav from '../components/Nav.svelte'
-	import Footer from '../components/Footer.svelte'
-</script>
-
-<style scoped>
-	br{
-		margin-top: 20px;
-	}
-
-	main {
-		min-height: 100vh;
-		min-width: 100vh;
-	}
-</style>
-
-<Nav/>
 <main>
 	<slot></slot>
 </main>
-
-<br>
-
-<Footer/>
