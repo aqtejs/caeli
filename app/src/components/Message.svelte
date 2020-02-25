8647698b823a106b4d8d31e8a6ea8609c6d3685a
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-02-25 18:47:55 +0100

    Added vertical margin

--- app/src/components/Message.svelte
+++ app/src/components/Message.svelte
@@ -6,13 +6,15 @@
     .box{
         border-radius: 40px;
         border-bottom-right-radius: 0;
+
+        margin: 10px 0;
     }
 </style>
 
 <div class="box is-rounded">
     {#if from}
     <label class="label">
-        {from.username}
+        {from.name}
         {#if from.id}
         <span class="tag is-primary is-light">{from.id}</span>
         {/if}
